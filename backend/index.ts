4c1fab50fd65d00bcc055b2b5ffecb971085f405
An Huynh <17773328+An-Huynh@users.noreply.github.com>
2022-12-21 10:15:01 -0600

    Add Dockerfile file to dockerize.

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,13 @@
 import express, { Express } from "express";
-import dotenv from "dotenv";
 import passport from "passport";
 import { localStrategy } from "./src/config/passport";
 import { authRouter } from "./src/routes";
 import { errorHandler } from "./src/middleware/error";
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-
 passport.use("local", localStrategy);
 
 const app: Express = express();
-const port: Number = Number(process.env.PORT);
+const PORT = 3000;
 
 app.use(express.json());
 
@@ -18,6 +15,6 @@ app.use("/auth", authRouter);
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`[Server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`[Server]: Server is running at http://localhost:${PORT}`);
 });
