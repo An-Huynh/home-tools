a877c57f101dad4d2223acbe590cc91b7c1ef740
An Huynh <17773328+An-Huynh@users.noreply.github.com>
2022-12-28 20:24:55 -0600

    Add loading indicator component.

---
+++ home-tools-web/src/components/LoadingIndicator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="spin"></div>
+</template>
+<style scoped lang="scss">
+@import "@/assets/styles/constants.scss";
+
+@keyframes spinner {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.spin::before {
+  animation: 1.5s linear infinite spinner;
+  animation-play-state: inherit;
+  border: solid 5px #cfd0d1;
+  border-bottom-color: $accent-color;
+  border-radius: 50%;
+  content: "";
+  height: 1rem;
+  width: 1rem;
+  will-change: transform;
+  display: inline-block;
+}
+</style>
